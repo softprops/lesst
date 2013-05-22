7259cb6c79cc3db83fbf144be5c34cecd49eddd1
softprops <d.tangren@gmail.com>
2013-05-21 20:46:56 -0400

    some modifications for lesst use

--- src/main/resources/less-rhino-1.4.0.js
+++ src/main/resources/less-rhino-1.4.0.js
@@ -4150,18 +4150,24 @@ tree.jsify = function (obj) {
 })(require('./tree'));
 var name;
 
+// reference to the name argument in compile function
+var rootPath, curPath, limports;
+
 function loadStyleSheet(sheet, callback, reload, remaining) {
-    var endOfPath = Math.max(name.lastIndexOf('/'), name.lastIndexOf('\\')),
-        sheetName = name.slice(0, endOfPath + 1) + sheet.href,
-        contents = sheet.contents || {},
-        input = readFile(sheetName);
-        
-    contents[sheetName] = input;
+//    var endOfPath = Math.max(name.lastIndexOf('/'), name.lastIndexOf('\\')),
+   var sheetName = curPath + '/' + sheet.href,
+       input = readFile(sheetName),
+       contents = sheet.contents || {};
+   contents[sheetName] = input;
         
     var parser = new less.Parser({
-        paths: [sheet.href.replace(/[\w\.-]+$/, '')],
+       // paths: [sheet.href.replace(/[\w\.-]+$/, '')],
         contents: contents
     });
+    var prevPath = curPath;
+    curPath = sheetName.slice(0, sheetName.lastIndexOf('/'));
+    limports.push(sheetName.slice(rootPath.length + 1));
+
     parser.parse(input, function (e, root) {
         if (e) {
             return error(e, sheetName);
@@ -4181,93 +4187,26 @@ function writeFile(filename, content) {
     out.close();
 }
 
-// Command line integration via Rhino
-(function (args) {
-    var output,
-        compress = false,
-        i;
-        
-    for(i = 0; i < args.length; i++) {
-        switch(args[i]) {
-            case "-x":
-                compress = true;
-                break;
-            default:
-                if (!name) {
-                    name = args[i];
-                } else if (!output) {
-                    output = args[i];
-                } else {
-                    print("unrecognised parameters");
-                    print("input_file [output_file] [-x]");
-                }
-        }
-    }
 
-    if (!name) {
-        print('No files present in the fileset; Check your pattern match in build.xml');
-        quit(1);
-    }
-    path = name.split("/");path.pop();path=path.join("/")
-
-    var input = readFile(name);
-
-    if (!input) {
-        print('lesscss: couldn\'t open file ' + name);
-        quit(1);
-    }
-
-    var result;
-    try {
-        var parser = new less.Parser();
-        parser.parse(input, function (e, root) {
-            if (e) {
-                error(e, name);
-                quit(1);
-            } else {
-                result = root.toCSS({compress: compress || false});
-                if (output) {
-                    writeFile(output, result);
-                    print("Written to " + output);
-                } else {
-                    print(result);
-                }
-                quit(0);
-            }
-        });
-    }
-    catch(e) {
-        error(e, name);
-        quit(1);
-    }
-    print("done");
-}(arguments));
+// removed original cmdline argument handling function
+// with this function for convenient scoped access to the
+// less.Parser function
+function compile(scriptName, code, min) {
+  if (!scriptName) throw { message: "empty script name " + scriptName };
+  curPath = rootPath = scriptName.slice(0, scriptName.lastIndexOf('/'));
+  limports = [];
+  var css = null;
+  new less.Parser().parse(code, function (e, root) {
+    if(e) { throw e; }// todo we could add scriptname here for more context
+    css = root.toCSS({ compress: min || false })
+  });
+  return new CompilationResult(css, limports);
+}
 
 function error(e, filename) {
-
-    var content = "Error : " + filename + "\n";
-    
-    filename = e.filename || filename;
-    
-    if (e.message) {
-        content += e.message + "\n";
-    }
-
-    var errorline = function (e, i, classname) {
-        if (e.extract[i]) {
-            content += 
-                String(parseInt(e.line) + (i - 1)) + 
-                ":" + e.extract[i] + "\n";
-        }
-    };
-
-    if (e.stack) {
-        content += e.stack;
-    } else if (e.extract) {
-        content += 'on line ' + e.line + ', column ' + (e.column + 1) + ':\n';
-        errorline(e, 0);
-        errorline(e, 1);
-        errorline(e, 2);
-    }
-   print(content);
+    // this function previously constructed a string to print
+    // here, we are adding the filename for context and
+    // rethrowing the error containing structured information
+    e.filename = filename;
+    throw e;
 }
