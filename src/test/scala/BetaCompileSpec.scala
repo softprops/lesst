97b09be5ff66380be51ce727340ade005a272919
softprops <d.tangren@gmail.com>
2013-05-21 22:56:41 -0400

    updated tests

--- src/test/scala/BetaCompileSpec.scala
+++ src/test/scala/BetaCompileSpec.scala
@@ -1,17 +1,19 @@
 package lesst
 
 import org.scalatest.FunSpec
+import scala.util.Success
 
 class BetaSpec extends FunSpec with Fixtures {
   describe ("beta compile") {
     it ("should compile basic less files") {
       val path = "less/basic.less"
       val code = file("/" + path)
-      Compile.beta(path, code).fold(fail(_), {
-        case CompilationResult(css, imports) =>
+      Compile.beta(path, code) match {
+        case Success(CompilationResult(css, imports)) =>
           assert(css === file("/css/basic.css"))
-          assert(imports === List())
-      })
+          assert(imports === List())          
+        case f => fail("expected success but was %s" format f)
+      }
     }
   }
 }
