4e126475471b05217a42ae3417269758d86eabcf
softprops <d.tangren@gmail.com>
2013-08-17 19:27:10 -0400

    keep the 2.10 version in source control

--- src/main/scala/sheets.scala
+++ src/main/scala/sheets.scala
@@ -5,8 +5,7 @@ import org.mozilla.javascript.{ NativeArray, ScriptableObject }
 case class StyleSheet(src: String, imports: List[String])
 
 class ScriptableStyleSheet extends ScriptableObject {
-  // fixme: use implicit class when dropping 2.9.2
-  implicit def NativeArrayWrapper(arr: NativeArray) = new {
+  implicit class NativeArrayWrapper(arr: NativeArray) {
     def toList[T](f: AnyRef => T): List[T] =
       (arr.getIds map { id: AnyRef =>
         f(arr.get(id.asInstanceOf[java.lang.Integer], null))
